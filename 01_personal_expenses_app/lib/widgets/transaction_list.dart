5272c2469542e038493977c5f17042ef39f38935
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2023-03-26 12:27:44 +0900

    Add transaction adding

--- 01_personal_expenses_app/lib/widgets/transaction_list.dart
+++ 01_personal_expenses_app/lib/widgets/transaction_list.dart
@@ -1,17 +1,60 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
-  const TransactionList({super.key});
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
 
-  @override
-  State<TransactionList> createState() => _TransactionListState();
-}
+  TransactionList(this.transactions);
 
-class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('\$${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                          color: Colors.purple,
+                        )),
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Text(DateFormat.yMMMEd().format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            )),
+                      ])
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
